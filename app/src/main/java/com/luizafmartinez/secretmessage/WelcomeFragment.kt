182526437893c8adce85abfe4770c69d87e07e4d
alxmart <alxmartinez@gmail.com>
2023-07-28 09:48:11 -0300

    Added a FragmentContainer replacing activity_main.xml items.

--- app/src/main/java/com/luizafmartinez/secretmessage/WelcomeFragment.kt
+++ app/src/main/java/com/luizafmartinez/secretmessage/WelcomeFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 class WelcomeFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
