b8b214ef3f271269b585e135526bc6c5aaf3eb95
alxmart <alxmartinez@gmail.com>
2023-07-31 14:37:31 -0300

    Added an onClickListener to WelcomeFragment button.

--- app/src/main/java/com/luizafmartinez/secretmessage/WelcomeFragment.kt
+++ app/src/main/java/com/luizafmartinez/secretmessage/WelcomeFragment.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+
 class WelcomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(
+        val view = inflater.inflate(
             R.layout.fragment_welcome,
             container,
             false
         )
+
+        val startButton = view.findViewById<Button>(R.id.start)
+
+        startButton.setOnClickListener {
+            // code that runs when the button is clicked
+        }
+        return view
     }
 }
